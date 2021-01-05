61709c4dc17cc0c61f21744f1439d8e00e2ecdec
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-01-05 21:09:08 +0500

    navigation to home page on logo click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/nike-logo.png';
 import './styles/navbar.scss';
 
 const Navbar: React.FC = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="nike" className="navbar--logo"/>
+      <Link to="/">
+        <img src={logo} alt="nike" className="navbar--logo"/>
+      </Link>
       <ul className="navbar--links">
         <li>New Releases</li>
         <li>Men</li>
